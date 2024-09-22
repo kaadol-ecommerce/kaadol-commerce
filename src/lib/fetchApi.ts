10ff11868895640579d1546891e04411a65cd765
mhenriette <mhenriette00@gmail.com>
2024-09-22 15:38:30 +0200

    feat: add typesb

---
+++ src/lib/fetchApi.ts
@@ -0,0 +1,20 @@
+import { fetchHttp } from "azle/src/lib/fetch/http";
+
+export async function fetchAPI<T>(path: string, options = {}) {
+    try {
+        const mergedOptions = {
+            next: { revalidate: 60 },
+            headers: {
+                "Content-Type": "application/json",
+            },
+            ...options,
+        };
+        const pathUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api${path}`;
+        const response = await fetch(pathUrl, mergedOptions);
+        const data = await response.json() as T;
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
