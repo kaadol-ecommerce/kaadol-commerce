8bf84be1bc4ed9dd704a189025ffaa8a44fe2c82
mhenriette <mhenriette00@gmail.com>
2024-09-22 17:07:28 +0200

    feat: add footer and other sections

--- src/components/cards/MotorAd.tsx
+++ src/components/cards/MotorAd.tsx
@@ -9,25 +9,18 @@ import {
 import Image from "next/image";
 
 export default function MotorAd({data}:any) {
-  console.log(data)
   return (
-    <Card>
-      <CardHeader>
-        <div className="relative w-full h-52 rounded-lg overflow-hidden">
-          <Image src="/wagon.jpg" alt="car" fill />
+    <div className="w-full rounded-md  border border-gray-200 hover:shadow-md">
+      <div className="relative w-full h-52">
+        <Image src="/wagon.jpg" alt="car" fill />
+      </div>
+      <div className="text-md pt-5 flex flex-col gap-1 p-4">
+        <span className="line-clamp-1">{data?.title}</span>
+        <span className="font-bold text-blue-950 text-lg">{data?.price} RWF</span>
+        <div>
+          <span>{data?.year} -</span> <span>{data?.kilometers} km</span>
         </div>
-      </CardHeader>
-      <CardContent>
-        <p className="font-bold text-blue-950 text-lg">{data?.price}</p>
-        <div className="text-sm">
-          <div>
-            <span>{data?.title}</span>
-          </div>
-          <div>
-            <span>{data?.year} -</span> <span>{data?.kilometers} km</span>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+      </div>
+    </div>
   );
 }
---
+++ src/components/cards/PlotAd.tsx
@@ -0,0 +1,23 @@
+import { Plot } from "@/types";
+import { MapPin } from "lucide-react";
+import Image from "next/image";
+
+export default function PlotAd({ data }:{data:Plot}) {
+  return (
+    <div className="w-full rounded-md  border border-gray-200 hover:shadow-md">
+      <div className="relative w-full h-52">
+        <Image src="/land.jpg" alt="car" fill />
+      </div>
+      <div className="text-md pt-5 flex flex-col gap-1 p-4">
+        <span className="line-clamp-1">{data?.title}</span>
+        <span className="font-bold text-blue-950 text-lg">
+          {data?.price} RWF
+        </span>
+        <div className="flex gap-1 items-center">
+          <MapPin color="#ff8906" />
+          <span>{data.location}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
